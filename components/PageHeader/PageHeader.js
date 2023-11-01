264d3c3a4cfce82acb1c8bb6267e91c81d4eabdb
jakebennett18505 <jake.bennett185@gmail.com>
2023-11-01 22:55:26 +0100

    Added SASS

--- components/PageHeader/PageHeader.js
+++ components/PageHeader/PageHeader.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import 'app/globals.css'
+import styles from '/sass/main.scss'
 
 const PageHeader = ({ pathname }) => {
   return (
     <div className="main">
-        <h1>This is the {pathname} page using next.js</h1>
+      <div className={styles.card}><h1>This is the {pathname} page using next.js</h1></div>
     </div>
   );
 };
