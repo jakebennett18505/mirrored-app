eac80051875d87af064ed3e854fd92a17036e083
jakebennett18505 <jake.bennett185@gmail.com>
2023-11-28 19:57:44 +0000

    Clearing svelte-kit

--- .svelte-kit/generated/client/nodes/8.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../src/routes/artist/[artistId]/gallery/+page.svelte";
--- .svelte-kit/generated/client/nodes/9.js
+++
@@ -1,3 +0,0 @@
-import * as universal from "../../../../src/routes/auth/login/+page.js";
-export { universal };
-export { default as component } from "../../../../src/routes/auth/login/+page.svelte";
