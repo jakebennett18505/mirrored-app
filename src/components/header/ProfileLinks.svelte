49ddb5ff6c560c0324bc0893d71a05970a785f20
Jake Bennett <jake.bennett185@gmail.com>
2023-12-07 21:23:35 +0100

    Cleanup

--- src/components/header/ProfileLinks.svelte
+++ src/components/header/ProfileLinks.svelte
@@ -1,77 +1,72 @@
 <script>
-  import { page } from "$app/stores"
-  import { signOut } from "../auth.js"
+	import { page } from '$app/stores'
+	import { signOut } from '../auth.js'
 
-  const { session, supabase } = $page.data
-  const profileLinks = ["Profile", "Settings"]
-  let container
-  let innerWidth
+	const { session, supabase } = $page.data
+	const profileLinks = ['Profile', 'Settings']
+	let container
+	let innerWidth
 
-  $: show = innerWidth < 1024
+	$: show = true
 
-  async function handleSignOut() {
-    await signOut(supabase)
-  }
+	async function handleSignOut() {
+		await signOut(supabase)
+	}
 
-  function toggleMenu() {
-    if (innerWidth > 1024) {
-      show = !show
-    }
-  }
+	function toggleMenu() {
+		if (innerWidth > 1024) {
+			show = !show
+		}
+	}
 
-  function onWindowClick(e) {
-    if (innerWidth > 1024 && show) {
-      if (container.contains(e.target) == false) show = false
-    }
-  }
+	function onWindowClick(e) {
+		if (innerWidth > 1024 && show) {
+			if (container.contains(e.target) == false) show = false
+		}
+	}
 </script>
 
 <svelte:window bind:innerWidth on:click={onWindowClick} />
 
 {#if !session}
-  <a
-    href="/auth/signup"
-    class="w-full gap-2 flex justify-between items-center rounded-lg p-3 text-2xl font-light leading-6 text-inherit lg:text-sm lg:w-fit hover:bg-neutral/5"
-    >Sign up <span class="hidden lg:inline" aria-hidden="true">&rarr;</span></a>
+	<a
+		href="/auth/signup"
+		class="w-full gap-2 flex justify-between items-center rounded-lg p-3 text-2xl font-light leading-6 text-inherit lg:text-sm lg:w-fit hover:bg-neutral/5"
+		>Sign up <span class="hidden lg:inline" aria-hidden="true">&rarr;</span></a>
 {:else}
-  <div
-    bind:this={container}
-    class="flex items-center gap-4 py-2 lg:p-0 lg:dropdown">
-    <button
-      on:click={toggleMenu}
-      tabindex="0"
-      class="rounded-full w-16 h-16 lg:w-8 lg:h-8 avatar">
-      <div class="rounded-full">
-        <img
-          alt="Tailwind CSS Navbar component"
-          src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-      </div>
-    </button>
-    <div class="inline-block lg:hidden">
-      <div class="text-xl">Jane Doe</div>
-      <div>
-        {session.user.email}
-      </div>
-    </div>
-  </div>
-  <!-- mt-2 lg:absolute lg:p-3 lg:-left-4 lg:top-full lg:z-10 lg:mt-3 lg:w-screen lg:max-w-[200px] lg:overflow-hidden lg:rounded-3xl lg:bg-base-100 dark:bg-inherit lg:shadow-lg lg:ring-1 lg:ring-gray-900/5 dark:lg:ring-gray-100/5 dark:shadow-gray-100/5 -->
-  {#if show}
-    <div
-      class="mt-2 bg-base-100 p-2 lg:absolute lg:z-1 lg:w-[200px] lg:rounded-3xl lg:shadow-2xl">
-      {#each profileLinks as link}
-        <a
-          href={link.toLowerCase()}
-          class="-ml-3 block rounded-lg px-3 py-2 text-base font-light leading-6 lg:text-sm hover:bg-neutral/5">
-          {link}
-        </a>
-      {/each}
-      <button
-        on:click={handleSignOut}
-        class="-ml-3 block rounded-lg px-3 py-2 text-base font-light leading-6 lg:text-sm hover:bg-neutral/5"
-        >Logout
-      </button>
-    </div>
-  {/if}
+	<div bind:this={container} class="flex items-center gap-4 py-2 lg:p-0 lg:dropdown">
+		<button on:click={toggleMenu} tabindex="0" class="rounded-full w-16 h-16 lg:w-8 lg:h-8 avatar">
+			<div class="rounded-full">
+				<img
+					alt="Tailwind CSS Navbar component"
+					src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+			</div>
+		</button>
+		<div class="inline-block lg:hidden">
+			<div class="text-xl">Jane Doe</div>
+			<div>
+				{session.user.email}
+			</div>
+		</div>
+	</div>
+	<!-- mt-2 lg:absolute lg:p-3 lg:-left-4 lg:top-full lg:z-10 lg:mt-3 lg:w-screen lg:max-w-[200px] lg:overflow-hidden lg:rounded-3xl lg:bg-base-100 dark:bg-inherit lg:shadow-lg lg:ring-1 lg:ring-gray-900/5 dark:lg:ring-gray-100/5 dark:shadow-gray-100/5 -->
+	{#if show}
+		<div
+			class="mt-2 flex flex-col bg-base-100 p-2 lg:absolute lg:mt-12 lg:z-1 lg:w-[200px] lg:rounded-3xl lg:shadow-2xl">
+			{#each profileLinks as link}
+				<a
+					href={link.toLowerCase()}
+					class="-ml-3 block rounded-lg px-3 py-2 text-base font-light leading-6 lg:m-0 lg:text-sm hover:bg-neutral/5">
+					{link}
+				</a>
+			{/each}
+			<button
+				on:click={handleSignOut}
+				class="text-left -ml-3 block rounded-lg px-3 py-2 text-base font-light leading-6 lg:m-0 lg:text-sm hover:bg-neutral/5"
+				>Logout
+			</button>
+		</div>
+	{/if}
 {/if}
 
 <style>
--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,22 +1,8 @@
 <script>
-  import { page } from "$app/stores"
+	import { page } from '$app/stores'
 
-  const session = $page.data.session
+	const session = $page.data.session
 
-  export let data
-  let { art } = data
-
-  function randomNumber() {
-    return Math.floor(Math.random() * 10)
-  }
-
-  let number = randomNumber()
-
-  let artwork = art[number]
+	export let data
+	let { art } = data
 </script>
-
-<div class="container">Hello</div>
-
-{#each artwork.images as image}
-  <img src={image} class="contain" alt="This was taken by me" />
-{/each}
